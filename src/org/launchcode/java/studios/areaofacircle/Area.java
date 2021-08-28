0312cd26584de889fb0441408cbc548325956c29
swankidelic <swankidelic@gmail.com>
2021-08-28 10:55:22 -0500

    completed bonus missions on studio 2.5

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -5,11 +5,22 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        boolean validInput = false;
+        double r = 0.0;
         System.out.println("Enter a radius: ");
-        String userRadius = input.nextLine();
-        Double r = Double.parseDouble(userRadius);
+        String userInput = input.nextLine();
+        while (!validInput) {
+            try {
+                r = Double.parseDouble(userInput);
+                validInput = true;
+            } catch (NullPointerException|NumberFormatException e) {
+                System.out.println("No valid input received. parseDouble returned '" + e + "'");
+                System.out.println("Please enter a radius: ");
+                userInput = input.nextLine();
+                validInput = false;
+            }
+        }
         input.close();
-
         System.out.println("The area of a circle of radius " + r + " is: " + Circle.getArea(r));
     }
 }
