281b2d3d52d2de009ef1c6dcb5cd2eedb7480ac2
swankidelic <swankidelic@gmail.com>
2021-09-27 13:48:13 -0500

    completed chapted 8 exersize, interfaces in ice cream menu

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -7,13 +7,37 @@ public class Main {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        ArrayList<Topping> toppings = menu.getToppings();
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
+        flavors.sort(new FlavorComparator());
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
+        cones.sort(new ConeComparator());
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        System.out.println("\nIce cream flavors\nSorted by number of allergens, least first\n~~~~~~~~~~~~");
+        String s = "";
+        for (Flavor flavor : flavors) {
+            if (flavor.getAllergens().size() != 1) s = "s";
+            System.out.println(flavor.getName() + ", " + flavor.getAllergens().size() + " allergen" + s);
+        }
+
+        System.out.println("\nIce cream cone styles\nSorted by cost, least first\n~~~~~~~~~~~~");
+        for (Cone cone : cones) {
+            System.out.println(cone.getName() + " $" + cone.getCost());
+        }
+
+        // Bonus mission: toppings and topping sorting
+
+        toppings.sort(new ToppingComparator());
+
+        System.out.println("\nIce cream toppings\nSorted alphabetically\n~~~~~~~~~~~~");
+        for (Topping topping : toppings) {
+            System.out.println(topping.getName());
+        }
+
     }
 }
